0a24041d444246875351b7e0c3da4a6a7fa0c21f
Swlo <ppaulo2003.ppsr@gmail.com>
2023-11-17 23:15:56 -0300

    finish DSW homework

--- Tree/src/main/java/com/swlo/tree/DswMain.java
+++ Tree/src/main/java/com/swlo/tree/DswMain.java
@@ -1,4 +1,62 @@
 package com.swlo.tree;
 
+import com.swlo.tree.trees.BinaryTree;
+import com.swlo.tree.util.PrintEnum;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class DswMain {
+
+
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            Integer number = random.nextInt(0, 100);
+            if (list.contains(number)) {
+                i--;
+            } else {
+                list.add(number);
+                tree.add(number);
+            }
+        }
+
+
+        System.out.println("\nArvoré não balanceada com 100 elementos");
+        System.out.println(tree.getOrdered(PrintEnum.LEVEL));
+
+
+        tree.balance();
+
+        System.out.println("\nArvoré  balanceada com 100 elementos");
+        System.out.println(tree.getOrdered(PrintEnum.LEVEL));
+
+
+        for (int i = 0; i < 20; i++) {
+            Integer number = random.nextInt(0, 200);
+            if (list.contains(number)) {
+                i--;
+            } else {
+                list.add(number);
+                tree.add(number);
+            }
+        }
+
+
+        System.out.println("\nArvoré  com 100 elementos balanceados + 20 elementos não balanceados");
+        System.out.println(tree.getOrdered(PrintEnum.LEVEL));
+
+        tree.balance();
+
+        System.out.println("\nArvoré balanceada com 120 elementos");
+        System.out.println(tree.getOrdered(PrintEnum.LEVEL));
+
+
+    }
 }
